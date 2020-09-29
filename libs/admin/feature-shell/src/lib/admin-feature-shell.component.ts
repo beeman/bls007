b3e71a0343fcdb853277625a1b90418a0380ade0
Bram Borggreve <borggreve@gmail.com>
2020-09-29 15:49:29 -0500

    feat: add style, layout and assets libs

--- libs/admin/feature-shell/src/lib/admin-feature-shell.component.ts
+++ libs/admin/feature-shell/src/lib/admin-feature-shell.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core'
 @Component({
   template: `
     <h1>
+      <img [attr.src]="logo" height="30" alt="" />
       Admin
       <small>
         &nbsp; <a routerLink="/dashboard">Dashboard</a> &nbsp; <a routerLink="/intercom">Intercom</a> &nbsp;
@@ -12,4 +13,7 @@ import { Component } from '@angular/core'
     <router-outlet></router-outlet>
   `,
 })
-export class AdminFeatureShellComponent {}
+export class AdminFeatureShellComponent {
+  logo = '/assets/images/logo.png'
+  // Determine what links we want to show
+}
---
+++ libs/admin/ui-style/README.md
@@ -0,0 +1,7 @@
+# admin-ui-style
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test admin-ui-style` to execute the unit tests.
